bd7fa35ba43be6430e1cf7bf8f0598825f7c4b42
notvedantt <142892853+notvedantt@users.noreply.github.com>
2024-08-17 12:57:02 +0530

    Add files via upload

---
+++ Day 1/listandtuple.py
@@ -0,0 +1,18 @@
+list1 = [10, 5.5, [3,6], "Code",] #Mutable (data can be changed)
+print(list1)
+
+tuple1 = ("Hello", "Goodbye",) #Immutable (data cannot be changed)
+print(tuple1)
+
+dict1 = {"name":"Vedant", "age":20, "education":"Btech"} #Collection of key:value pair
+print(dict1)
+
+#everything in python is object oriented
+
+
+#OUTPUT
+'''
+[10, 5.5, [3, 6], 'Code']
+('Hello', 'Goodbye')
+{'name': 'Vedant', 'age': 20, 'education': 'Btech'}
+'''
