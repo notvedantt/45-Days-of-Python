622fe6abaad8bd8bc9d5cc840f91870ef2fac31d
notvedantt <142892853+notvedantt@users.noreply.github.com>
2024-08-17 11:32:15 +0530

    Add files via upload
    DAY 0

---
+++ Day 0/commentsescape.py
@@ -0,0 +1,7 @@
+# This is a single-line comment
+print("Hey , I'm Vedant Kalas \nand 19 yrs old") #Escape sequence character \n
+
+"""
+This is Multi-line
+comment
+"""
---
+++ Day 0/first.py
@@ -0,0 +1,7 @@
+
+#Installing Modules (terminal)
+
+#pip command
+#pip install pandas
+#pip install scikit-learn(ML library)
+#REPL (Read evaluate print loop)
