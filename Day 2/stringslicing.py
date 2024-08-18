217e75232653dce575913796429be18570964227
notvedantt <142892853+notvedantt@users.noreply.github.com>
2024-08-18 23:53:48 +0530

    Add files via upload

---
+++ Day 2/stringslicing.py
@@ -0,0 +1,19 @@
+bike = "Kawasaki"
+Length = len(bike) #Determine the lenth for string
+print(Length)
+
+print(bike[0:4])
+print(bike[1:4])
+print(bike[:5])
+print(bike[0:-3])
+print(bike[-3:-1])
+
+#OUTPUT
+'''
+8
+Kawa
+awa
+Kawas
+Kawas
+ak
+'''
