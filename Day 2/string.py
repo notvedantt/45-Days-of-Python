e63a55520538693060b89bfa48376fafc11769f0
notvedantt <142892853+notvedantt@users.noreply.github.com>
2024-08-18 23:53:24 +0530

    Add files via upload

---
+++ Day 2/string.py
@@ -0,0 +1,40 @@
+name = "Vedant"
+print("Hello," + name)
+
+statement = "He said, \"I want to buy a car" #Escape command used to add double quotes in string
+print(statement)
+
+
+#Multiline string
+a = ''' lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do '''
+print(a)
+
+#Indexing
+print (name[0])
+
+#STRING IS LIKE** A ARRAY OF CHARACTER
+
+#Looping through a string
+for character in name:
+    print(character)
+
+
+
+#OUTPUT
+'''
+Hello,Vedant
+He said, "I want to buy a car
+ lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do 
+V
+#For loop output
+V
+e
+d
+a
+n
+t
+'''
