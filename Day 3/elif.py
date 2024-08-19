590f0e018b32fbb6694a4de601d0cf6e5abf9f4b
notvedantt <142892853+notvedantt@users.noreply.github.com>
2024-08-19 21:52:38 +0530

    Add files via upload

---
+++ Day 3/elif.py
@@ -0,0 +1,25 @@
+a = int(input("Enter the number: "))
+print("Your number is ", a)
+
+if(a>0):
+    print("Number is positive.")
+elif(a==0):
+    print("Number is zero.")
+else:
+    print("Number is negative.")
+
+#OUTPUT
+''' 
+1.Enter the number: 10
+Your number is  10
+Number is positive.
+
+2.Enter the number: 0
+Your number is  0
+Number is zero.
+
+3.Enter the number: -3
+Your number is  -3
+Number is negative.
+'''
+     
