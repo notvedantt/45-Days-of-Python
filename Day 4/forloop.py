2fd2296187b8369574c814c009cca6b444f9e538
notvedantt <142892853+notvedantt@users.noreply.github.com>
2024-08-24 02:55:07 +0530

    Add files via upload

---
+++ Day 4/forloop.py
@@ -0,0 +1,34 @@
+#FOR LOOP
+name = "Pyhton"
+for i in name:
+    print(i)
+'''
+OUTPUT
+P
+y
+h
+t
+o
+n
+'''
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+    print(color)
+'''OUTPUT
+Red
+Green
+Blue
+Yellow
+'''
+    
+#RANGE
+for i in range(5):
+    print(i)
+
+for i in range(5):
+    print(i + 1) #Adds one to the starting value 
+
+for i in range(11,20):
+    print(i) #Prints the given range
+    
