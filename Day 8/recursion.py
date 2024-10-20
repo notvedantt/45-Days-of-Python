0703f3edd62094dd125c15a1045d382e12c9bdeb
notvedantt <142892853+notvedantt@users.noreply.github.com>
2024-10-20 22:43:08 +0530

    Update recursion.py

--- Day 8/recursion.py
+++ Day 8/recursion.py
@@ -2,7 +2,7 @@ def factorial(num):
     if(num == 1 or num == 0):
         return 1
     else:
-        return (num * factorial(num - 1))
+        return (num * factorial(num - 1)) #factorial num - 1 i.e 7*factorial of 6 
 
 print(factorial(3))
 #Can add same function in existing function 
