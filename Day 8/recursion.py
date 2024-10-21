3b508d232c2201649569bcfbe3770cb36e035a5f
notvedantt <142892853+notvedantt@users.noreply.github.com>
2024-10-21 19:19:42 +0530

    recursion.py

--- Day 8/recursion.py
+++ Day 8/recursion.py
@@ -4,5 +4,17 @@ def factorial(num):
     else:
         return (num * factorial(num - 1)) #factorial num - 1 i.e 7*factorial of 6 
 
-print(factorial(3))
-#Can add same function in existing function 
+print(factorial(7))
+
+
+def fibonacci(n):
+    if(n == 0):
+        return 0
+    elif(n == 1):
+        return 1
+    elif(n>1):
+        return (fibonacci(n-1) + fibonacci(n-2)) #Can add same function in existing function 
+    
+print(fibonacci(7))
+
+
